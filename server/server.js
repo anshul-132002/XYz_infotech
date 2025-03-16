07586dff5d12ad9dcecca8924b332bd87a36f4b6
Anshul shrivas <128448038+anshul-132002@users.noreply.github.com>
2025-03-16 19:44:38 +0530

    added render website

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import "dotenv/config";
+
 import express from "express";
 import cors from "cors";
 import { router } from "./routes/auth-router.js"; // Import the router from auth-router.js
@@ -9,20 +10,18 @@ import { contactForm } from "./controller/contact-controller.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
-// CORS options with correct spelling and deployed frontend URL
-const corsOption = {
+//!middlewre thaat pares incoming requests in json payload
+const corsOption={
   origin: ["http://localhost:5173", "https://xyz-infotech-frontend.onrender.com"], // Ensure correct production frontend URL
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
-  credentials: true, // Correct key
-};
-
-app.use(cors(corsOption));
+  credentials: true,
+}
+app.use(cors(corsOption))
 app.use(express.json());
 app.use("/api", router);
-app.use("/form", contactForm);
+app.use("/form",contactForm)
 app.use(errorMiddleware);
 
-// Connect DB and start server
 db().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on ${port}`);
